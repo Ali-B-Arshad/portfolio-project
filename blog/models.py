557623bcc4507eedc7f0107293291e9b9a970809
Ali-B-Arshad <ali.b.arshadd@gmail.com>
2019-08-03 03:29:49 +0500

    portfolio up and running

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blogs(models.Model):
     Publication_date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     Body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.Body[:100]
+
+    def Publication_date_pretty(self):
+        return self.Publication_date.strftime('%e %B %Y')
