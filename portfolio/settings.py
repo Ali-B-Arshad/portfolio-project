c670d98270825a89f8d92f859838895dad3578cf
Ali-B-Arshad <ali.b.arshadd@gmail.com>
2019-08-03 04:21:21 +0000

    tweaks

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'Wateen!@#',
+        'PASSWORD': 'Wateen!@#123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
